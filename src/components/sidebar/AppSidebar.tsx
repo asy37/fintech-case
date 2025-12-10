e4c4124eb928bc272e1e8b5c9e7358ac6fc934a0
asy37 <ahmetsylmz37@gmail.com>
2025-12-10 04:03:02 +0300

    sidebar responsive

--- src/components/sidebar/AppSidebar.tsx
+++ src/components/sidebar/AppSidebar.tsx
@@ -21,82 +21,84 @@ export const AppSidebar = () => {
   const path = usePathname()
 
   return (
-    <Sidebar className="bg-snow flex items-center justify-between px-6 pt-[30px] pb-[100px]">
-      <SidebarHeader>
+    <Sidebar className="bg-snow px-6 pt-[30px] pb-[100px]">
+      <SidebarHeader className='md:p-2 p-12'>
         <Image src="/Logo.svg" alt="logo" height={30} width={107} />
       </SidebarHeader>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarMenu>
-            {mockSidebar.map((item) => {
-              const isActive = item.href === path
-              return (
-                <SidebarMenuItem key={item.id}>
-                  <Link href={item.href}>
+      <div className="flex flex-col h-full items-center md:justify-between">
+        <div>
+          <SidebarGroup>
+            <SidebarMenu>
+              {mockSidebar.map((item) => {
+                const isActive = item.href === path
+                return (
+                  <SidebarMenuItem key={item.id}>
+                    <Link href={item.href}>
+                      <SidebarMenuButton
+                        className={cn(
+                          'text-neutral-gray h-12 w-[200px] cursor-pointer rounded-md p-3.5 text-sm',
+                          isActive &&
+                            'bg-lime-green text-midnight-blue font-semibold',
+                        )}
+                      >
+                        <span className="h-5 w-5">
+                          <item.icon
+                            className={cn(
+                              'stroke-neutral-gray fill-neutral-gray',
+                              isActive && 'fill-midnight-blue',
+                            )}
+                          />
+                        </span>
+                        <span>{item.title}</span>
+                      </SidebarMenuButton>
+                    </Link>
+                  </SidebarMenuItem>
+                )
+              })}
+            </SidebarMenu>
+          </SidebarGroup>
+        </div>
+        <div>
+          <SidebarContent>
+            <SidebarGroup>
+              <SidebarMenu>
+                <SidebarMenuItem>
+                  <Link href="/help">
                     <SidebarMenuButton
                       className={cn(
                         'text-neutral-gray h-12 w-[200px] cursor-pointer rounded-md p-3.5 text-sm',
-                        isActive &&
+                        path === '/help' &&
                           'bg-lime-green text-midnight-blue font-semibold',
                       )}
                     >
                       <span className="h-5 w-5">
-                        <item.icon
+                        <HelpIcon
                           className={cn(
                             'stroke-neutral-gray fill-neutral-gray',
-                            isActive && 'fill-midnight-blue',
+                            path === '/help' && 'fill-midnight-blue',
                           )}
                         />
                       </span>
-                      <span>{item.title}</span>
+                      <span>Help</span>
                     </SidebarMenuButton>
                   </Link>
                 </SidebarMenuItem>
-              )
-            })}
-          </SidebarMenu>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarFooter>
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <Link href="/help">
+                <SidebarMenuItem>
                   <SidebarMenuButton
-                    className={cn(
-                      'text-neutral-gray h-12 w-[200px] cursor-pointer rounded-md p-3.5 text-sm',
-                      path === '/help' &&
-                        'bg-lime-green text-midnight-blue font-semibold',
-                    )}
+                    onClick={() => console.log('logout')}
+                    className="text-neutral-gray h-12 w-[200px] cursor-pointer rounded-md p-3.5 text-sm"
                   >
                     <span className="h-5 w-5">
-                      <HelpIcon
-                        className={cn(
-                          'stroke-neutral-gray fill-neutral-gray',
-                          path === '/help' && 'fill-midnight-blue',
-                        )}
-                      />
+                      <LogoutIcon className="fill-neutral-gray" />
                     </span>
-                    <span>Help</span>
+                    <span>Logout</span>
                   </SidebarMenuButton>
-                </Link>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton
-                  onClick={() => console.log('logout')}
-                  className="text-neutral-gray h-12 w-[200px] cursor-pointer rounded-md p-3.5 text-sm"
-                >
-                  <span className="h-5 w-5">
-                    <LogoutIcon className="fill-neutral-gray" />
-                  </span>
-                  <span>Logout</span>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroup>
-        </SidebarContent>
-      </SidebarFooter>
+                </SidebarMenuItem>
+              </SidebarMenu>
+            </SidebarGroup>
+          </SidebarContent>
+        </div>
+      </div>
     </Sidebar>
   )
 }
